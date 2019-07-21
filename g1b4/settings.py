53a0b4a50e7cc84fe95b36bc76e6fb7c05a6805f
donghyun <donghyun4012@naver.com>
2019-07-21 13:50:57 +0900

    host ++

--- g1b4/settings.py
+++ g1b4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '-eb+6#hd7y5-p!j-mbc)y=ss+ss8-b
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
