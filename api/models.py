e61aba1129f10e9cb59041b6559b607a8aaca1f2
donghyun <donghyun4012@naver.com>
2019-08-01 18:39:41 +0900

    multi and description ++

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pictures(models.Model):
     picture = models.ImageField(null=True, default=None, blank=True)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
 class Post(models.Model):
     message = models.TextField()
