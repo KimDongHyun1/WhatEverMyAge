420f179decc590348dec2da0590a42958adc7a25
donghyun <donghyun4012@naver.com>
2019-08-01 22:18:19 +0900

    C:/Program Files/Git/ ---

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('question/', include('question.urls')),
     path('users/', include('users.urls')),
-    path('a/', UserUploadedPicture.as_view()),
+    path('a', UserUploadedPicture.as_view()),
 
 ]
 
